a0cd97f8e81746afb87496839a1f8f6f70aa9e39
Yeh-chi Lai <yehchilai@csupomona.edu>
2017-01-11 13:36:53 -0800

    refactor: remove duplicate

--- spec/AddressBookSpec.js
+++ spec/AddressBookSpec.js
@@ -1,14 +1,22 @@
 describe('Address book', function(){
+
+  var addressBook;
+  var contact;
+
+  beforeEach(function(){
+    addressBook = new AddressBook();
+    contact = new Contact();
+  });
+
+
   it('should be able to add a contact', function(){
-    var addressBook = new AddressBook();
-    var contact = new Contact();
+
     addressBook.addContact(contact);
     expect(addressBook.getContact(0)).toBe(contact);
   });
 
   it('should be able to delete a contact',function(){
-    var addressBook = new AddressBook();
-    var contact = new Contact();
+
     addressBook.addContact(contact);
     addressBook.deleteContact(0);
     expect(addressBook.getContact(0)).not.toBeDefined();
