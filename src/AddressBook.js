d9880262d9593c28c45e261261455f389bf8c2c0
Yeh-chi Lai <yehchilai@csupomona.edu>
2017-01-11 13:41:31 -0800

    add: Testing Asynchronous

--- src/AddressBook.js
+++ src/AddressBook.js
@@ -1,10 +1,22 @@
 function AddressBook(){
   this.contacts=[];
+  this.initialComplete = false;
 }
 /* The same with the following */
 // var AddressBook = function (){
 //   this.contacts=[];
 // }
+
+AddressBook.prototype.getInitialContacts = function(cb) {
+  var self = this;
+
+  setTimeout(function(){
+    self.initialComplete = true;
+    if(cb){
+      return cb();
+    }
+  },30);
+};
 
 AddressBook.prototype.addContact = function(Contact) {
   this.contacts.push(Contact);
